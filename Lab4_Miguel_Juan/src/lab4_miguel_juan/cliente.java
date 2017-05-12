e09d5de1f6f6abc7def286af5b7d060a307ad7d0
Miguel Flores <mike97@hotmail.es>
2017-05-12 14:00:32 -0600

    Creacion de las clases principales

--- Lab4_Miguel_Juan/src/lab4_miguel_juan/cliente.java
+++ Lab4_Miguel_Juan/src/lab4_miguel_juan/cliente.java
@@ -17,4 +17,43 @@ public class cliente extends Personas {
     private Date primer_prdocto;
     private int cantidad_productos;
 
+    public cliente(double presupuesto, Date primer_prdocto, int cantidad_productos, String nombre, String id, int edad, double altura, double peso, String residencia) {
+        super(nombre, id, edad, altura, peso, residencia);
+        this.presupuesto = presupuesto;
+        this.primer_prdocto = primer_prdocto;
+        this.cantidad_productos = cantidad_productos;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public Date getPrimer_prdocto() {
+        return primer_prdocto;
+    }
+
+    public void setPrimer_prdocto(Date primer_prdocto) {
+        this.primer_prdocto = primer_prdocto;
+    }
+
+    public int getCantidad_productos() {
+        return cantidad_productos;
+    }
+
+    public void setCantidad_productos(int cantidad_productos) {
+        this.cantidad_productos = cantidad_productos;
+    }
+
+    @Override
+    public String toString() {
+        return "cliente{" + "presupuesto=" + presupuesto + ", primer_prdocto=" + primer_prdocto + ", cantidad_productos=" + cantidad_productos + '}';
+    }
+    
+    
+    
+
 }
