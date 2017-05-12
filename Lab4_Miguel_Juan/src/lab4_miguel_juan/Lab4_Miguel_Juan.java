3efef1017f9abcf6d3082a6af45f61771c45ae9d
Juan Guevara <Juan Guevara>
2017-05-12 13:24:18 -0600

    commit inicial

---
+++ Lab4_Miguel_Juan/src/lab4_miguel_juan/Lab4_Miguel_Juan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_miguel_juan;
+
+/**
+ *
+ * @author HP
+ */
+public class Lab4_Miguel_Juan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("l");
+    }
+    
+}
